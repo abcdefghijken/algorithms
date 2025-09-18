8c34e2286cc2f4bf4a159525e6688bf988d3030a
Ken <k-en@live.com.sg>
2025-09-18 22:49:30 +0800

    Done some Codility questions

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import codility.BinaryGap;
+import codility.Chapter1;
+import codility.CyclicRotation;
+import grokking.BinarySearch;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+//        BinarySearch binarySearch = new BinarySearch();
+//
+//        System.out.println(binarySearch.useBinarySearch(binarySearch.getBinarySearchTestArr1(), 69));
+
+//        Chapter1 chapter1 = new Chapter1();
+//        chapter1.doTriangle();
+
+//        BinaryGap binaryGap = new BinaryGap();
+//        binaryGap.doBinaryGap(143);
+
+        CyclicRotation cyclicRotation = new CyclicRotation();
+        cyclicRotation.doCyclicRotation(cyclicRotation.testCase1, 3);
+    }
+}
